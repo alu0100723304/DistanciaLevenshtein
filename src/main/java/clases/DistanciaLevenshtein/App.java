0157fae4ea8730b55a749fc86fd5b3c1b64d015e
Joey <Joey@192.168.1.35>
2016-12-02 20:03:39 +0000

    Subiendo implementacion del algoritmo de Distancia de Levenshtein

--- src/main/java/clases/DistanciaLevenshtein/App.java
+++ src/main/java/clases/DistanciaLevenshtein/App.java
@@ -4,10 +4,9 @@ package clases.DistanciaLevenshtein;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Prueba01: " + LevenshteinDistance.computeLevenshteinDistance("oslo", "oslo"));
+		System.out.println("Prueba02: " + LevenshteinDistance.computeLevenshteinDistance("cama", "caixa"));
+	}
 }
