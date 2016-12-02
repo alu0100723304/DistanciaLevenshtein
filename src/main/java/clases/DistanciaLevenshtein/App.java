3c77a17474dbb6f2de5fd03c8a2e33b98e4a3ede
Joey <Joey@192.168.1.35>
2016-12-02 20:55:36 +0000

    Probando el codigo implementado

--- src/main/java/clases/DistanciaLevenshtein/App.java
+++ src/main/java/clases/DistanciaLevenshtein/App.java
@@ -7,6 +7,7 @@ package clases.DistanciaLevenshtein;
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Prueba01: " + LevenshteinDistance.computeLevenshteinDistance("oslo", "oslo"));
-		System.out.println("Prueba02: " + LevenshteinDistance.computeLevenshteinDistance("cama", "caixa"));
+		System.out.println("Prueba02: " + LevenshteinDistance.computeLevenshteinDistance("pata", "gata"));
+		System.out.println("Prueba03: " + LevenshteinDistance.computeLevenshteinDistance("moto", "oso"));
 	}
 }
