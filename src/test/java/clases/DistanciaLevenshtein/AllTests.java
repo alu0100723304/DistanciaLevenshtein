d31e2723d615921f484c0cdca3191c1bad807863
Joey <Joey@192.168.1.35>
2016-12-03 00:42:16 +0000

    Subiendo Test de JUnit

--- src/test/java/clases/DistanciaLevenshtein/AllTests.java
+++ src/test/java/clases/DistanciaLevenshtein/AllTests.java
@@ -5,12 +5,13 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 /**
- * @author Joel Perez Ramos
+ * @author Joel Perez Ramos 
  * AllTest de la Distancia de Levenshtein.
  */
 @RunWith(Suite.class)
 @SuiteClasses({AppTest.class, LevenshteinDistanceTest01.class,
-		LevenshteinDistanceTest02.class, LevenshteinDistanceTest03.class})
+		LevenshteinDistanceTest02.class, LevenshteinDistanceTest03.class,
+		LevenshteinDistanceTest04.class})
 public class AllTests {
 
 }
